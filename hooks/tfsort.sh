12c08bebc4414067ab86ce0ac126f8b8d1e08e4f
Vincent Norguet <vincent@neutronium.tech>
2024-01-10 00:46:44 +0100

    feat(tfsort): Update commit

--- hooks/tfsort.sh
+++ hooks/tfsort.sh
@@ -12,8 +12,69 @@ function main {
   common::parse_cmdline "$@"
   common::export_provided_env_vars "${ENV_VARS[@]}"
   common::parse_and_export_env_vars
-  # shellcheck disable=SC2153 # False positive
-  tfsort "${HOOK_CONFIG[*]}" "${ARGS[*]}" "${FILES[@]}"
+
+  tfsort_ "${HOOK_CONFIG[*]}" "${ARGS[*]}" "${FILES[@]}"
+  # for target_file in "${FILES[@]}"; do
+  #   echo "Running tfsort on ${FILES[@]}"
+  #   tfsort "${ARGS[@]}" "${target_file}"
+
+  # done
 }
 
-[ "${BASH_SOURCE[0]}" != "$0" ] || main "$@"
+# function tfsort {
+#   for target_file in "${FILES[@]}"; do
+#     echo "Running tfsort on ${FILES[@]}"
+#     tfsort "${ARGS[@]}" "${target_file}"
+#   done
+# }
+
+# [ "${BASH_SOURCE[0]}" != "$0" ] || main "$@"
+
+
+# function main {
+#   common::initialize "$SCRIPT_DIR"
+#   common::parse_cmdline "$@"
+#   common::export_provided_env_vars "${ENV_VARS[@]}"
+#   common::parse_and_export_env_vars
+
+#   # shellcheck disable=SC2153 # False positive
+#   for target_file in "${FILES[@]}"; do
+#     echo "Running tfsort on ${target_file}"
+#     tfsort "${ARGS[@]}" "${target_file}"
+#   done
+# }
+
+# #######################################################################
+# # Unique part of `common::per_dir_hook`. The function is executed in loop
+# # on each provided dir path. Run wrapped tool with specified arguments
+# # Arguments:
+# #   dir_path (string) PATH to dir relative to git repo root.
+# #     Can be used in error logging
+# #   change_dir_in_unique_part (string/false) Modifier which creates
+# #     possibilities to use non-common chdir strategies.
+# #     Availability depends on hook.
+# #   args (array) arguments that configure wrapped tool behavior
+# # Outputs:
+# #   If failed - print out hook checks status
+# #######################################################################
+# function tfsort_ {
+#   # shellcheck disable=SC2034 # Unused var.
+#   # local -r dir_path="$1"
+#   # shellcheck disable=SC2034 # Unused var.
+#   # local -r change_dir_in_unique_part="$2"
+#   # shift 2
+#   local -a -r args=("$@")
+
+#   # pass the arguments to hook
+#   for target_file in "${args[@]}"; do
+#     echo "Running tfsort on ${target_file}"
+#     tfsort "${ARGS[@]}" "${target_file}"
+#   done
+#   # tfsort "${args[@]}"
+
+#   # return exit code to common::per_dir_hook
+#   local exit_code=$?
+#   return $exit_code
+# }
+
+# [ "${BASH_SOURCE[0]}" != "$0" ] || main "$@"
